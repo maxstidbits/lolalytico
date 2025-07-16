9af8ff5bf5d423e4b174dfb64cf684a55e492c60
Kacper Kowalski <xperski@pm.me>
2025-07-16 22:37:16 +0200

    auto-commit

--- src/lolalytics_api/__init__.py
+++ src/lolalytics_api/__init__.py
@@ -2,7 +2,7 @@ from .main import get_tierlist, get_counters, _sort_by_rank, display_lanes, disp
     patch_notes
 from .errors import InvalidLane, InvalidRank
 
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 __author__ = "xPerSki"
 
 __all__ = ["get_tierlist", "get_counters", "display_lanes", "display_ranks", "InvalidRank", "InvalidLane",
